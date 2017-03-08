7000cee295f94813c491fae6e81fddcffb857a79
Umberto Raimondi <me@uraimo.com>
2017-03-08 23:07:43 +0100

    Improved example

--- Examples/Raspi2/Sources/main.swift
+++ Examples/Raspi2/Sources/main.swift
@@ -8,16 +8,18 @@ let pwm = (pwms[0]?[.P18])!
 let s1 = SG90Servo(pwm)
 s1.enable()
 
-print("<- Left")
-s1.move(to: .left)
-sleep(10)
+for i in 0...5 {
+    print("<- Left")
+    s1.move(to: .left)
+    sleep(1)
 
-print("^  Middle")
-s1.move(to: .middle)
-sleep(10)
+    print("^  Middle")
+    s1.move(to: .middle)
+    sleep(1)
 
-print("-> Right")
-s1.move(to: .right)
-sleep(10)
+    print("-> Right")
+    s1.move(to: .right)
+    sleep(1)
+}
 
 s1.disable()
