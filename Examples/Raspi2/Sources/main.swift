1144f4a787fad7dc17ac5a795ec1434544dc0d0f
Umberto Raimondi <me@uraimo.com>
2017-03-08 22:40:51 +0100

    Added output to example

--- Examples/Raspi2/Sources/main.swift
+++ Examples/Raspi2/Sources/main.swift
@@ -7,11 +7,17 @@ let pwm = (pwms[0]?[.P18])!
 
 let s1 = SG90Servo(pwm)
 s1.enable()
+
+print("<- Left")
 s1.move(to: .left)
-sleep(1)
+sleep(10)
+
+print("^  Middle")
 s1.move(to: .middle)
-sleep(1)
+sleep(10)
+
+print("-> Right")
 s1.move(to: .right)
-sleep(1)
+sleep(10)
 
 s1.disable()
