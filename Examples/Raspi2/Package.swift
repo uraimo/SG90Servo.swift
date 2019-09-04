197aceabb2279404ceba0209021758fd8754cb03
Umberto Raimondi <me@uraimo.com>
2019-09-04 09:05:59 +0200

    Fix example, still has dependency declaration issues

--- Examples/Raspi2/Package.swift
+++ Examples/Raspi2/Package.swift
@@ -1,26 +1,13 @@
-import PackageDescription
-
-let package = Package(
-    name: "TestServo",
-    dependencies: [
-        .Package(url: "https://github.com/uraimo/SG90Servo.swift.git",
-                 majorVersion: 2)
-    ]
-)
-
-
 // swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "TestServo",
     dependencies: [
-        .package(url: "https://github.com/uraimo/SG90Servo.swift.git",
-                 , from: "3.0.0"),
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/uraimo/SG90Servo.swift.git",from: "3.0.0")
     ],
     targets: [
-        .target(
-            name: "TestServo",
-            dependencies: ["SG90Servo.swift"])
+        .target(name: "TestServo", dependencies: ["SwiftyGPIO","SG90Servo.swift"]),
     ]
 ) 
