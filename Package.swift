1de353e333a88bc1c6a2f01dc042a1e84790bfdc
Umberto Raimondi <me@uraimo.com>
2019-09-04 09:18:57 +0200

    Fixed package declaration

--- Package.swift
+++ Package.swift
@@ -3,12 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "SG90Servo",
+    products: [
+        .library(name: "SG90Servo", targets: ["SG90Servo"]),
+    ], 
     dependencies: [
         .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
     ],
     targets: [
-        .target(
-            name: "SG90Servo",
-            dependencies: ["SwiftyGPIO"])
+        .target(name: "SG90Servo", dependencies: ["SwiftyGPIO"]),
     ]
 )
